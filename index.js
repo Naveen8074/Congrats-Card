c3e3d444085ff60dce6786d819290ce44a01cb49
Naveen8074 <sodabattinanaveen@gmail.com>
2024-11-10 05:48:38 +0000

    adding all files

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className='text_div_contaciner'>
+    <h1 className='heading'>Congratulations</h1>
+    <div className='blue-div'>
+      <img
+        className='imagg'
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png'
+      />
+      <h1 className='heading'>Kiran V</h1>
+      <p className='para'>
+        Vishun institute of computer eductaion and technology, bhimavaram
+      </p>
+      <img
+        className='imagg'
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
